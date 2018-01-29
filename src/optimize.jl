8c56def7825446270327add3a863e18a261a3806
Zenna Tavares <zennatavares@gmail.com>
2018-01-29 11:53:47 -0500

    tensorboard issues

--- src/optimize.jl
+++ src/optimize.jl
@@ -16,18 +16,6 @@ function init_placeholders(arr::Arrow)
   intens, ph_pid
 end
 
-# function genstep(iters, indata, ph_pid,)
-#   function step!()
-#     indata = AlioAnalysis.take1(iters)
-#     phsvalmap = populate(indata, ph_pid)
-#     cur_loss, _ = run(sess, [meanloss, minimize_op], phsvalmap)
-#     summaries = run(sess, merged_summary_op, phsvalmap)
-#     write(summary_writer, summaries, i)
-#     cur_loss
-#     i = i + 1
-#     cur_loss
-#   end
-# end
 
 """
 argmin_θ(ϵprt): find θ which minimizes ϵprt
@@ -44,6 +32,7 @@ function optimize(carr::CompArrow, # Redundant? #FIXME, remove
                   ϵprt::Port,
                   ingen,
                   target=Type{TFTarget};
+                  testevery = 10,
                   testingen = ingen,
                   logdir::String = log_dir(),
                   kwargs...)
@@ -58,7 +47,6 @@ function optimize(carr::CompArrow, # Redundant? #FIXME, remove
 
   ## FIXME: Can't we break this up?
   tf.as_default(graph) do
-    showgraphstats(graph)
     # Create input tensor for reach in port and mapping between port ids and placeholders
     intens, ph_pid = init_placeholders(carr)
     tfarr = Graph(carr, graph, intens)
@@ -66,37 +54,37 @@ function optimize(carr::CompArrow, # Redundant? #FIXME, remove
     losses = tfarr.out[ϵid]
 
     meanloss = tf.reduce_mean(losses) #FIXME: Should param
-    trainloss = tf.reduce_mean(losses) # FIXME HACK shouldnt need two losses
     optimizer = tf.train.AdamOptimizer() #FIXME: Should be param
-    minimize_op = tf.train.minimize(optimizer, meanloss) # Should be param
-    mean_train_loss = summary.scalar("Mean Train Loss", meanloss)
-    mean_test_loss = summary.scalar("Mean Test Loss", trainloss)
+    min_op = tf.train.minimize(optimizer, meanloss) # Should be param
+    mean_train_loss = summary.scalar("Loss", meanloss)
     merged_summary_op = summary.merge_all() #
-    summary_writer = summary.FileWriter(logdir) # FIXME Should be opt?
-    showgraphstats(graph)
-    run(sess, global_variables_initializer())
+    summary_writer = summary.FileWriter(joinpath(logdir, "train"))
+    test_writer = summary.FileWriter(joinpath(logdir, "test"))
     
+    run(sess, global_variables_initializer())   
     function step!(cb_data, callbacks)
-      indata = AlioAnalysis.take1(ingen)
-      phsvalmap = populate(indata, ph_pid)
-      cur_loss, _ = run(sess, [meanloss, minimize_op], phsvalmap)
-
+      if cb_data.i % testevery != 0
+        indata = AlioAnalysis.take1(ingen)
+        ph_to_val = populate(indata, ph_pid)
+        cur_loss, _, summaries = run(sess, [meanloss, min_op, merged_summary_op], ph_to_val)
+        write(summary_writer, summaries, cb_data.i)
+        @show cur_loss
+        cb_data_ = merge(cb_data, @NT(loss = cur_loss))
+        foreach(cb->cb(cb_data_), callbacks)
+        cur_loss  
       # HACK
-      if testingen != ingen
+      elseif testingen != ingen
         # Don't minimize
         testindata = AlioAnalysis.take1(testingen)
-        phsvalmap = populate(testindata, ph_pid)
-        train_loss = run(sess, [trainloss], phsvalmap)[1]
+        ph_to_val = populate(testindata, ph_pid)
+        train_loss, summaries = run(sess, [meanloss, merged_summary_op], ph_to_val)
         println("Trainloss, ", train_loss)
+        write(test_writer, summaries, cb_data.i)
+        train_loss
       end
-
-      @show cur_loss
-      summaries = run(sess, merged_summary_op, phsvalmap)
-      write(summary_writer, summaries, cb_data.i)
-      cb_data_ = merge(cb_data, @NT(loss = cur_loss))
-      foreach(cb->cb(cb_data_), callbacks)
-      cur_loss
+      # summaries = run(sess, merged_summary_op, ph_to_val)
     end
+    showgraphstats(graph)
     return optimize(step!; kwargs...)
   end
 end
